dc6cbbaefb830a3cd9ea9ff071b2c710f7a71c7e
Abdullah Sayed <abdsayed@lab3r1s5.42abudhabi.ae>
2024-10-02 20:20:03 +0400

    finally

--- Pipex_bonus/here_doc.c
+++ Pipex_bonus/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: abdsayed <abdsayed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 17:54:48 by abdsayed          #+#    #+#             */
-/*   Updated: 2024/10/02 19:35:36 by abdsayed         ###   ########.fr       */
+/*   Updated: 2024/10/02 20:19:13 by abdsayed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,16 @@ void	secondfork(t_pipex *pipex)
 		dup2(pipex->pipes[0], 0);
 		dup2(pipex->fdo, 1);
 		(close(pipex->fdo), close(pipex->pipes[0]), close(pipex->pipes[1]));
-		if (ft_strchr(pipex->av[3], '/') != NULL)
-			pipex->token = pipex->av[3];
+		if (ft_strchr(pipex->av[4], '/') != NULL)
+			pipex->token = pipex->av[4];
 		else if (pipex->bole == 0)
-			findtoken(pipex, pipex->av[3]);
+			findtoken(pipex, pipex->av[4]);
 		else
 			exitfree("Error14", pipex, 1);
-		exec(pipex->token, pipex->av[3], pipex->env, pipex);
+		exec(pipex->token, pipex->av[4], pipex->env, pipex);
 		exitfree("", pipex, 1);
 	}
-	return ;
+	close(0), close(1);
 }
 
 void	startforking(t_pipex *pipex)
@@ -44,22 +44,22 @@ void	startforking(t_pipex *pipex)
 	pid = fork();
 	if (pid == 0)
 	{
-		dup2(pipex->docpipe[0], 0);
+		dup2(pipex->docpipe[0], STDIN_FILENO);
 		close(pipex->docpipe[0]);
-		dup2(pipex->pipes[1], 1);
+		dup2(pipex->pipes[1], STDOUT_FILENO);
 		(close(pipex->pipes[1]), close(pipex->pipes[0]));
-		fprintf(STDIN_FILENO, "%i\n", pipex->bole);
-		if (ft_strchr(pipex->av[2], '/') != NULL)
-			pipex->token = ft_strdup(pipex->av[2]);
+		if (ft_strchr(pipex->av[3], '/') != NULL)
+			pipex->token = ft_strdup(pipex->av[3]);
 		else if (pipex->bole == 0)
-			findtoken(pipex, pipex->av[2]);
+			findtoken(pipex, pipex->av[3]);
 		else
 			exitfree("Error16", pipex, 1);
 		if (pipex->token == NULL)
 			exitfree("Error17", pipex, 1);
-		exec(pipex->token, pipex->av[2], pipex->env, pipex);
+		exec(pipex->token, pipex->av[3], pipex->env, pipex);
 		exitfree("", pipex, 0);
 	}
+	close(pipex->docpipe[0]);
 }
 
 void	here_doc(t_pipex *pipex)
@@ -69,17 +69,21 @@ void	here_doc(t_pipex *pipex)
 	if (pipe(pipex->docpipe) == -1)
 		exitfree("Error18", pipex, 1);
 	str = get_next_line(0);
-	while (ft_strnstr(str, pipex->av[2], ft_strlen(str)) == NULL)
+	int len = ft_strlen(pipex->av[2]);
+	pipex->av[2][len] = '\n';
+	while (ft_strncmp(pipex->av[2], str, len + 1))
 	{
-		ft_printf("%s\n", str);
+		dprintf(STDERR_FILENO, "%s\n", str);
+		if (write(pipex->docpipe[1], str, ft_strlen(str)) < 0)
+			exitfree("Error", pipex, 1);
 		free(str);
 		str = get_next_line(0);
-		write(pipex->docpipe[1], str, ft_strlen(str));
 	}
+	dprintf(STDERR_FILENO, "%s\n", "OUTSIDE");
 	free(str);
 	close(pipex->docpipe[1]);
 	startforking(pipex);
-	(close(pipex->docpipe[0]), close(pipex->docpipe[1]));
+	(close(pipex->docpipe[0]));
 	secondfork(pipex);
-	close(0);
+	close(pipex->pipes[1]), close(pipex->pipes[0]);	
 }
